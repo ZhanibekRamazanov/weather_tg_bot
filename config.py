44aa7f62a5181c3b283267ab79c4abf485fa12cf
ZhanibekRamazanov <144475521+ZhanibekRamazanov@users.noreply.github.com>
2023-09-16 18:52:26 +0600

    Update config.py

--- config.py
+++ config.py
@@ -1,2 +1,2 @@
-open_weather_token = "18d751a2ecbe542ca7ffed2d7621b36a"
-tg_bot_token = "6546055656:AAFq3J1xkY7v7bM8s_jRZbQ7GcvlZlruZOA"
+open_weather_token = "your_token"
+tg_bot_token = "your_token"
